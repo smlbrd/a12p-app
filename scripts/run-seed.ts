908ab292a5d4fce10f21ec990aac01c4aa2b1140
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-06-18 15:26:12 +0100

    update seed script

--- scripts/run-seed.ts
+++ scripts/run-seed.ts
@@ -1,7 +1,12 @@
 import { seedCoinsAndDuties } from "../src/db/seeds/seedData.ts"
-import { pool } from "../src/db/db.ts"
+import { db, pool } from "../src/db/db.ts"
+import { coins, coinsToDuties, duties } from "../src/db/schema.ts"
 
 try {
+  console.log("🔥 Clearing database...")
+  await db.delete(coinsToDuties)
+  await db.delete(duties)
+  await db.delete(coins)
   console.log("🌱 Seeding coins...")
   await seedCoinsAndDuties()
   console.log("✅ Database seeded successfully!")
