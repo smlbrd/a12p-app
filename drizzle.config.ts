a25de61338404d2169f990c5917ae1bea0224be3
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-06-19 10:57:23 +0100

    edit drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://test_user:test_password@localhost:5433/test_db"
+    host: process.env.PG_HOST!,
+    port: Number(process.env.DB_PORT) || 25060,
+    user: process.env.PG_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 })
