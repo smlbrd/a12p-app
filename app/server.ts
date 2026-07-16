bdcc90ebf7a32c57404e0b6b0a057fd42e90cc74
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-07-16 09:38:20 +0100

    simplify server root

--- app/server.ts
+++ app/server.ts
@@ -3,14 +3,13 @@ import { errorHandler } from "./middleware/errorHandler.ts"
 import { secureHeaders } from "hono/secure-headers"
 import { handle } from "hono/aws-lambda"
 import { serveStatic } from "@hono/node-server/serve-static"
-import { fileURLToPath } from "node:url"
 
 const app = createApp()
 
 app.use("*", secureHeaders())
 app.onError(errorHandler)
 
-app.use("/static/*", serveStatic({ root: fileURLToPath(new URL("./", import.meta.url)) }))
+app.use("/static/*", serveStatic({ root: "./" }))
 
 export const handler = handle(app)
 
