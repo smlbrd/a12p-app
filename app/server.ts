630aef23c699be22e5e7e37a0c0798d0e87850d6
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-07-16 09:15:36 +0100

    fix container root

--- app/server.ts
+++ app/server.ts
@@ -3,13 +3,14 @@ import { errorHandler } from "./middleware/errorHandler.ts"
 import { secureHeaders } from "hono/secure-headers"
 import { handle } from "hono/aws-lambda"
 import { serveStatic } from "@hono/node-server/serve-static"
+import { fileURLToPath } from "node:url"
 
 const app = createApp()
 
 app.use("*", secureHeaders())
 app.onError(errorHandler)
 
-app.use("/static/*", serveStatic({ root: "./" }))
+app.use("/static/*", serveStatic({ root: fileURLToPath(new URL("./", import.meta.url)) }))
 
 export const handler = handle(app)
 
