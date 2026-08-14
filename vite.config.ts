6f9d29e445d7e070bebb5493772201ca996c8f48
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-08-14 15:17:39 +0100

    add env to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import honox from "honox/vite"
 import client from "honox/vite/client"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    Object.assign(process.env, env)
+
     if (mode === "client") {
         return {
             plugins: [client(), tailwindcss()],
